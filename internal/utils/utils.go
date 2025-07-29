0df2568ae0b292fc73c036dd6624925252b01f68
tenmo <tenmo2003@gmail.com>
2025-07-29 15:19:06 +0700

    add path expansion for image path

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -5,9 +5,11 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 	"os"
+	"strings"
 )
 
 func GetImageFromFilePath(filePath string) (image.Image, error) {
+	filePath = ExpandPath(filePath)
 	f, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
@@ -15,4 +17,21 @@ func GetImageFromFilePath(filePath string) (image.Image, error) {
 	defer f.Close()
 	image, _, err := image.Decode(f)
 	return image, err
+}
+
+func ExpandPath(filePath string) string {
+	if strings.HasPrefix(filePath, "~") {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			panic(err)
+		}
+		filePath = strings.Replace(filePath, "~", homeDir, 1)
+	} else if strings.HasPrefix(filePath, ".") {
+		currentDir, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+		filePath = strings.Replace(filePath, ".", currentDir, 1)
+	}
+	return filePath
 }
